f9627a81e835dc88a7fc35fda7d6c344b76b7081
YourVendor <yourvendor@ya.ru>
2025-04-13 18:01:16 +0300

    в локал завел бэк

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer
-from fastapi.middleware.cors import CORSMiddleware  # Добавь
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import jwt
 
 app = FastAPI()
-
-# Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rub2b.ru", "https://www.rub2b.ru"],  # Разреши оба домена
+    allow_origins=["http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-SECRET_KEY = "твой_секретный_ключ_смени_потом"
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-DATABASE_URL = "mysql+mysqlconnector://u3054650_germush:Gremushka27112007@localhost/u3054650_rub2b"
+SECRET_KEY = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6"
+DATABASE_URL = "postgresql+psycopg2://germush:Gremushka27112007@localhost/rub2b"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
@@ -44,6 +40,8 @@ def get_db():
 class UserIn(BaseModel):
     login: str
     password: str
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 @app.get("/")
 def root():
